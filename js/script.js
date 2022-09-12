784a0fa691b966395e8013cef704c4cc39690bf4
QuiqueSilva2022 <infotiempoactual@gmail.com>
2022-09-11 23:15:13 -0300

    Ejercicio 9 c Java

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_ycontinente);
